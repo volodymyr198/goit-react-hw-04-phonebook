3a7b0b4ad27fa4f03880f47674c3fcc70b253b90
volodymyr198 <volodymyr.b.198@gmail.com>
2023-02-01 23:28:41 +0200

    fixed

--- src/components/FormPhonebook/FormPhonebook.jsx
+++ src/components/FormPhonebook/FormPhonebook.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { PropTypes } from 'prop-types';
 
 import css from './FormPhonebook.module.css';
@@ -6,13 +6,13 @@ import css from './FormPhonebook.module.css';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
-class FormPhonebook extends Component {
-    handleSubmit = (values, { resetForm }) => {
-        this.props.onSubmit(values);
+const FormPhonebook = ({ onSubmit }) => {
+    const handleSubmit = (values, { resetForm }) => {
+        onSubmit(values);
         resetForm();
     };
 
-    validationSchema = Yup.object().shape({
+    const validationSchema = Yup.object().shape({
         name: Yup.string()
             .min(2, 'Too Short!')
             .max(16, 'Too Long!')
@@ -26,56 +26,55 @@ class FormPhonebook extends Component {
             .required('A phone number is required'),
     });
 
-    initialValues = {
+    const initialValues = {
         name: '',
         number: '',
     };
 
-    render() {
-        return (
-            <Formik
-                initialValues={this.initialValues}
-                validationSchema={this.validationSchema}
-                onSubmit={this.handleSubmit}
-            >
-                <Form className={css.formPhone}>
-                    <label className={css.formLabel}>
-                        Name
-                        <Field
-                            className={css.formInput}
-                            type="text"
-                            name="name"
-                            placeholder="Name"
-                        />
-                        <ErrorMessage
-                            className={css.error}
-                            name="name"
-                            component="div"
-                        />
-                    </label>
-                    <label className={css.formLabel}>
-                        Number
-                        <Field
-                            className={css.formInput}
-                            type="tel"
-                            name="number"
-                            pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
-                            placeholder="123 456 78 90"
-                        />
-                        <ErrorMessage
-                            className={css.error}
-                            name="number"
-                            component="div"
-                        />
-                    </label>
-                    <button className={css.formBtn} type="submit">
-                        Add contact
-                    </button>
-                </Form>
-            </Formik>
-        );
-    }
-}
+    return (
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+        >
+            <Form className={css.formPhone}>
+                <label className={css.formLabel}>
+                    Name
+                    <Field
+                        className={css.formInput}
+                        type="text"
+                        name="name"
+                        placeholder="Name"
+                    />
+                    <ErrorMessage
+                        className={css.error}
+                        name="name"
+                        component="div"
+                    />
+                </label>
+                <label className={css.formLabel}>
+                    Number
+                    <Field
+                        className={css.formInput}
+                        type="tel"
+                        name="number"
+                        pattern="[0-9]{3} [0-9]{3} [0-9]{4}"
+                        title="Phone number must be in the format 123 456 7890"
+                        placeholder="123 456 7890"
+                    />
+                    <ErrorMessage
+                        className={css.error}
+                        name="number"
+                        component="div"
+                    />
+                </label>
+                <button className={css.formBtn} type="submit">
+                    Add contact
+                </button>
+            </Form>
+        </Formik>
+    );
+};
 
 FormPhonebook.propTypes = {
     onSubmit: PropTypes.func.isRequired,
